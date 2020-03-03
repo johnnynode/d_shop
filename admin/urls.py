b04815b7a57b70307236768876bf0445efd8eead
johnnynode <johnnynode@gmail.com>
2020-03-03 18:46:51 +0800

    update: admin users

--- admin/urls.py
+++ admin/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
-from admin.views import index
+from admin.views import index, users
 
 urlpatterns = [
     # 后台首页
-    url(r'^$', index, name="admin_index"),
+    url(r'^$', index.index, name="admin_index"),
+    # 后台用户管理
+    url(r'^users$', users.index, name="admin_users_index"),
+    url(r'^users/add$', users.add, name="admin_users_add"),
+    url(r'^users/insert$', users.insert, name="admin_users_insert"),
+    url(r'^users/del/(?P<uid>[0-9]+)$', users.delete, name="admin_users_del"),
+    url(r'^users/edit/(?P<uid>[0-9]+)$', users.edit, name="admin_users_edit"),
+    url(r'^users/update/(?P<uid>[0-9]+)$', users.update, name="admin_users_update"),
 ]
