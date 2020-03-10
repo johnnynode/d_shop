fd462b4891bb9f728b1656c7d0ca0741a732a63b
johnnynode <johnnynode@gmail.com>
2020-03-10 21:44:23 +0800

    update: admin orders users

--- admin/urls.py
+++ admin/urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls import url
-from admin.views import index, users, type, goods
+
+from admin.views import index,users,type,goods,orders
 
 urlpatterns = [
     # 后台首页
     url(r'^$', index.index, name="admin_index"),
-    # 后台用户管理
-    url(r'^users$', users.index, name="admin_users_index"),
-    url(r'^users/add$', users.add, name="admin_users_add"),
-    url(r'^users/insert$', users.insert, name="admin_users_insert"),
-    url(r'^users/del/(?P<uid>[0-9]+)$', users.delete, name="admin_users_del"),
-    url(r'^users/edit/(?P<uid>[0-9]+)$', users.edit, name="admin_users_edit"),
-    url(r'^users/update/(?P<uid>[0-9]+)$', users.update, name="admin_users_update"),
 
     # 后台管理员路由
     url(r'^login$', index.login, name="admin_login"),
@@ -18,7 +12,17 @@ urlpatterns = [
     url(r'^logout$', index.logout, name="admin_logout"),
     url(r'^verify$', index.verify, name="admin_verify"), #验证码
 
-    # 后台商品类别信息管理
+    # 会员信息管理路由
+    url(r'^users/(?P<pIndex>[0-9]+)$', users.index, name="admin_users_index"),
+    url(r'^users/add$', users.add, name="admin_users_add"),
+    url(r'^users/insert$', users.insert, name="admin_users_insert"),
+    url(r'^users/del/(?P<uid>[0-9]+)$', users.delete, name="admin_users_del"),
+    url(r'^users/edit/(?P<uid>[0-9]+)$', users.edit, name="admin_users_edit"),
+    url(r'^users/update/(?P<uid>[0-9]+)$', users.update, name="admin_users_update"),
+    url(r'^users/resetpass/(?P<uid>[0-9]+)$', users.resetpass, name="admin_users_resetpass"),
+    url(r'^users/doresetpass/(?P<uid>[0-9]+)$', users.doresetpass, name="admin_users_doresetpass"),
+
+     # 商品类别信息管理路由
     url(r'^type$', type.index, name="admin_type_index"),
     url(r'^type/add/(?P<tid>[0-9]+)$', type.add, name="admin_type_add"),
     url(r'^type/insert$', type.insert, name="admin_type_insert"),
@@ -26,11 +30,17 @@ urlpatterns = [
     url(r'^type/edit/(?P<tid>[0-9]+)$', type.edit, name="admin_type_edit"),
     url(r'^type/update/(?P<tid>[0-9]+)$', type.update, name="admin_type_update"),
 
-    # 后台商品信息管理
+    # 商品信息管理路由
     url(r'^goods/(?P<pIndex>[0-9]+)$', goods.index, name="admin_goods_index"),
     url(r'^goods/add$', goods.add, name="admin_goods_add"),
     url(r'^goods/insert$', goods.insert, name="admin_goods_insert"),
     url(r'^goods/del/(?P<gid>[0-9]+)$', goods.delete, name="admin_goods_del"),
     url(r'^goods/edit/(?P<gid>[0-9]+)$', goods.edit, name="admin_goods_edit"),
     url(r'^goods/update/(?P<gid>[0-9]+)$', goods.update, name="admin_goods_update"),
+
+    #订单管理
+    url(r'^orders$', orders.index, name="admin_orders_index"),
+    url(r'^orders/(?P<pIndex>[0-9]+)$', orders.index, name="admin_orders_index"),
+    url(r'^orders/detail/(?P<oid>[0-9]+)$', orders.detail, name="admin_orders_detail"),
+    url(r'^orders/state$',orders.state, name="admin_orders_state"),
 ]
--- admin/views/users.py
+++ admin/views/users.py
@@ -1,23 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from common.models import Users
+from django.db.models import Q
+from django.core.paginator import Paginator
 from datetime import datetime
 
-# 浏览会员
-def index(request):
-    # 执行数据查询，并放置到模板中
-    list = Users.objects.all()
-    context = {"userslist":list}
-    #return HttpResponse(list)
-    return render(request,'admin/users/index.html',context)
+from common.models import Users
+# Create your views here.
+def index(request,pIndex=1):
+    '''浏览信息'''
+    umod = Users.objects
+    mywhere=[]
 
-# 会员信息添加表单
+    # 获取、判断并封装关keyword键搜索
+    kw = request.GET.get("keyword",None)
+    if kw:
+        # 查询账户或真实姓名中只要含有关键字的都可以
+        list = umod.filter(Q(username__contains=kw) | Q(name__contains=kw))
+        mywhere.append("keyword="+kw)
+    else:
+        list = umod.filter()
+    # 获取、判断并封装性别sex搜索条件
+    sex = request.GET.get('sex','')
+    if sex != '':
+        list = list.filter(sex=sex)
+        mywhere.append("sex="+sex)
+
+    #执行分页处理
+    pIndex = int(pIndex)
+    page = Paginator(list,5) #以5条每页创建分页对象
+    maxpages = page.num_pages #最大页数
+    #判断页数是否越界
+    if pIndex > maxpages:
+        pIndex = maxpages
+    if pIndex < 1:
+        pIndex = 1
+    list2 = page.page(pIndex) #当前页数据
+    plist = page.page_range   #页码数列表
+    
+
+    #封装信息加载模板输出
+    context = {"userslist":list2,'plist':plist,'pIndex':pIndex,'maxpages':maxpages,'mywhere':mywhere}
+    return render(request,"admin/users/index.html",context)
+
 def add(request):
-    return render(request,'admin/users/add.html')
+    '''加载添加页面'''
+    return render(request,"admin/users/add.html")
 
-#执行会员信息添加    
 def insert(request):
+    '''执行添加'''
     try:
         ob = Users()
         ob.username = request.POST['username']
@@ -35,36 +65,36 @@ def insert(request):
         ob.state = 1
         ob.addtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         ob.save()
-        context = {'info':'添加成功！'}
+        context={"info":"添加成功！"}
     except Exception as err:
         print(err)
-        context = {'info':'添加失败！'}
-
+        context={"info":"添加失败"}
     return render(request,"admin/info.html",context)
 
-# 执行会员信息删除
 def delete(request,uid):
+    '''删除信息'''
     try:
         ob = Users.objects.get(id=uid)
         ob.delete()
-        context = {'info':'删除成功！'}
-    except:
-        context = {'info':'删除失败！'}
-    return render(request,"admin/info.html",context)
-
-# 打开会员信息编辑表单
-def edit(request,uid):
-    try:
-        ob = Users.objects.get(id=uid)
-        context = {'user':ob}
-        return render(request,"admin/users/edit.html",context)
+        context={"info":"删除成功！"}
     except Exception as err:
         print(err)
-        context = {'info':'没有找到要修改的信息！'}
+        context={"info":"删除失败"}
     return render(request,"admin/info.html",context)
 
-# 执行会员信息编辑
+
+def edit(request,uid):
+    '''加载编辑信息页面'''
+    try:
+        ob = Users.objects.get(id=uid)
+        context={"user":ob}
+        return render(request,"admin/users/edit.html",context)
+    except Exception as err:
+        context={"info":"没有找到要修改的信息！"}
+        return render(request,"admin/info.html",context)
+
 def update(request,uid):
+    '''执行编辑信息'''
     try:
         ob = Users.objects.get(id=uid)
         ob.name = request.POST['name']
@@ -75,8 +105,34 @@ def update(request,uid):
         ob.email = request.POST['email']
         ob.state = request.POST['state']
         ob.save()
-        context = {'info':'修改成功！'}
+        context={"info":"修改成功！"}
     except Exception as err:
         print(err)
-        context = {'info':'修改失败！'}
+        context={"info":"修改失败"}
+    return render(request,"admin/info.html",context)
+
+def resetpass(request,uid):
+    '''加载重置会员密码信息页面'''
+    try:
+        ob = Users.objects.get(id=uid)
+        context={"user":ob}
+        return render(request,"admin/users/resetpass.html",context)
+    except Exception as err:
+        context={"info":"没有找到要修改的信息！"}
+        return render(request,"admin/info.html",context)
+
+def doresetpass(request,uid):
+    '''执行编辑信息'''
+    try:
+        ob = Users.objects.get(id=uid)
+        #获取密码并md5
+        import hashlib
+        m = hashlib.md5() 
+        m.update(bytes(request.POST['password'],encoding="utf8"))
+        ob.password = m.hexdigest()
+        ob.save()
+        context={"info":"密码重置成功！"}
+    except Exception as err:
+        print(err)
+        context={"info":"密码重置失败"}
     return render(request,"admin/info.html",context)
