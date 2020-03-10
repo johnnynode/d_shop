c2cc70b4acf495846d7e232fe8c9796c26c252f6
johnnynode <johnnynode@gmail.com>
2020-03-10 19:42:53 +0800

    update: web list detail

--- web/urls.py
+++ web/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # 前台首页
     url(r'^$', index.index, name="index"),	#商城首页
     url(r'^list$', index.lists, name="list"),# 商品列表
-    #url(r'^list/(?P<pIndex>[0-9]+)$',index.lists,name="list"), #分页商品列表展示
+    url(r'^list/(?P<pIndex>[0-9]+)$',index.lists,name="list"), #分页商品列表展示
     url(r'^detail/(?P<gid>[0-9]+)$', index.detail, name="detail"),#商品详情
 
     # 会员及个人中心等路由配置
--- web/views/index.py
+++ web/views/index.py
@@ -2,21 +2,76 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator
 
-from common.models import Users
+from common.models import Users, Types, Goods
+
+# 公共信息加载
+def loadinfo(request):
+    '''公共信息加载'''
+    context = {}
+    lists = Types.objects.filter(pid=0)
+    context['typelist'] = lists
+    return context
 
 # =============商品展示========================
 def index(request):
     '''项目前台首页'''
-    return render(request,"web/index.html")
+    context = loadinfo(request)
+    return render(request,"web/index.html",context)
 
 def lists(request,pIndex=1):
-    '''商品列表页'''
-    return render(request,"web/list.html")
+    '''商品列表页(搜索&分页)'''
+    context = loadinfo(request)
+    #获取商品信息查询对象
+    mod = Goods.objects
+    mywhere=[] #定义一个用于存放搜索条件列表
+
+    #判断添加搜索条件
+    tid = int(request.GET.get('tid',0))
+    if tid > 0:
+        list = mod.filter(typeid__in=Types.objects.only('id').filter(pid=tid))
+        mywhere.append("tid="+str(tid))
+    else:
+        list = mod.filter()
+
+    #获取、判断并封装关keyword键搜索
+    kw = request.GET.get("keyword",None)
+    if kw:
+        # 查询商品名中只要含有关键字的都可以
+        list = list.filter(goods__contains=kw)
+        mywhere.append("keyword="+kw)
+
+    #执行分页处理
+    pIndex = int(pIndex)
+    page = Paginator(list,5) #以5条每页创建分页对象
+    maxpages = page.num_pages #最大页数
+    #判断页数是否越界
+    if pIndex > maxpages:
+        pIndex = maxpages
+    if pIndex < 1:
+        pIndex = 1
+    list2 = page.page(pIndex) #当前页数据
+    plist = page.page_range   #页码数列表
+
+    #封装信息加载模板输出
+    context['goodslist'] = list2
+    context['plist'] = plist
+    context['pIndex'] = pIndex
+    context['maxpages'] = maxpages
+    context['mywhere'] = mywhere
+    context['tid'] = int(tid)
+    return render(request,"web/list.html",context)
 
 def detail(request,gid):
     '''商品详情页'''
-    return render(request,"web/detail.html")
+    context = loadinfo(request)
+    #加载商品详情信息
+    ob = Goods.objects.get(id=gid)
+    ob.clicknum += 1 # 点击量加1
+    ob.save()
+    context['goods'] = ob
+    return render(request,"web/detail.html",context)
 
 # ==============前台会员登录====================
 def login(request):
