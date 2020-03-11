32ac7b94a472771b71d19c6f21f10d89c69ea522
johnnynode <johnnynode@gmail.com>
2020-03-11 23:50:48 +0800

    update: admin index & types

--- admin/views/type.py
+++ admin/views/type.py
@@ -5,16 +5,35 @@ from django.core.urlresolvers import reverse
 
 from common.models import Types
 
+from django.db.models import Q
+from django.core.paginator import Paginator
+from datetime import datetime
+
 # 浏览商品类别信息
-def index(request):
+def index(request, pIndex=1):
     # 执行数据查询，并放置到模板中
     list = Types.objects.extra(select = {'_has':'concat(path,id)'}).order_by('_has')
+    
     # 遍历查询结果，为每个结果对象追加一个pname属性，目的用于缩进标题
     for ob in list:
         ob.pname ='. . . '*(ob.path.count(',')-1)
         # print(list[0].__dict__)
-    context = {"typeslist":list}
-    return render(request,'admin/type/index.html',context)
+    
+    #执行分页处理
+    pIndex = int(pIndex)
+    page = Paginator(list,5) #以5条每页创建分页对象
+    maxpages = page.num_pages #最大页数
+    #判断页数是否越界
+    if pIndex > maxpages:
+        pIndex = maxpages
+    if pIndex < 1:
+        pIndex = 1
+    list2 = page.page(pIndex) #当前页数据
+    plist = page.page_range   #页码数列表
+
+    #封装信息加载模板输出
+    context = {"typeslist":list2,'plist':plist,'pIndex':pIndex,'maxpages':maxpages}
+    return render(request,"admin/type/index.html",context)
 
 # 商品类别信息添加表单
 def add(request,tid):
